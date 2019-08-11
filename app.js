7f5d70053bf977085be0cedc6ae4a58d05b91c78
Patryk Wydmuch <pvydmuch@gmail.com>
2019-08-11 22:51:55 +0200

    Finish job

--- app.js
+++ app.js
@@ -5,12 +5,3 @@ $(window).on("scroll", () => {
       $('nav').removeClass('white');
     }
   })
-
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
